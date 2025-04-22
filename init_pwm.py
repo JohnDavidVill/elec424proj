d2723eb192d2a9e980c86afaf0e2c9df28898ad5
JohnDavidVill <107506620+JohnDavidVill@users.noreply.github.com>
2025-04-22 10:21:34 -0500

    Create init_pwm.py

---
+++ init_pwm.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+GPIO.setup(12, GPIO.OUT)
+
+def blink():
+    p = GPIO.PWM(12, 0.5)
+    p.start(1)
+    input('Press return to stop:')   # use raw_input for Python 2
+    p.stop()
+    GPIO.cleanup()
+
+def intensity():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.OUT)
+
+    p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+    p.start(0)
+    try:
+        while 1:
+            for dc in range(0, 101, 5):
+                p.ChangeDutyCycle(dc)
+                time.sleep(0.1)
+            for dc in range(100, -1, -5):
+                p.ChangeDutyCycle(dc)
+                time.sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    p.stop()
+    GPIO.cleanup()
+
+blink()
+# intensity();
